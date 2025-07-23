84a49393db3514524141bd2a616a49bb9c7a28d4
mantis-has <mantisbotmd@gmail.com>
2025-07-23 03:39:53 -0400

    Actualizar gc-kick.js

--- plugins/gc-kick.js
+++ plugins/gc-kick.js
@@ -11,20 +11,20 @@ var handler = async (m, { conn, participants, usedPrefix, command }) => {
     //const nn = conn.getName(m.sender);
 
     if (user === conn.user.jid) {
-        return conn.reply(m.chat, '🚩 No puedo eliminar el bot del grupo', m, rcanal, );
+        return conn.reply(m.chat, '🩵 No puedo eliminar el bot del grupo', m, fake, );
     }
 
     if (user === ownerGroup) {
-        return conn.reply(m.chat, '🚩 No puedo eliminar al propietario del grupo', m, rcanal, );
+        return conn.reply(m.chat, '🩵 No puedo eliminar al propietario del grupo', m, fake, );
     }
 
     if (user === ownerBot) {
-        return conn.reply(m.chat, '🚩 No puedo eliminar al propietario del bot', m, rcanal );
+        return conn.reply(m.chat, '🩵 No puedo eliminar al propietario del bot', m, fake );
     }
 
     await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
 
-//conn.reply('525544876071@s.whatsapp.net', `🚩 Un Admin Acabo De Eliminar Un Usuario En El Grupo:\n> ${groupMetadata.subject}.`, m, rcanal, );
+//conn.reply('525544876071@s.whatsapp.net', `🩵 Un Admin Acabo De Eliminar Un Usuario En El Grupo:\n> ${groupMetadata.subject}.`, m, fake, );
 };
 
 handler.help = ['kick'];
