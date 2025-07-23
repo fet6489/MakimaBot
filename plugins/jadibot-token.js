64e9ff1161720f4511601906903c56ec9363ef14
mantis-has <mantisbotmd@gmail.com>
2025-07-23 07:30:54 -0400

    Actualizar jadibot-token.js

--- plugins/jadibot-token.js
+++ plugins/jadibot-token.js
@@ -6,10 +6,10 @@ const user = m.sender.split('@')[0]
 if (fs.existsSync(`./${jadi}/` + user + '/creds.json')) {
 let token = Buffer.from(fs.readFileSync(`./${jadi}/` + user + '/creds.json'), 'utf-8').toString('base64')    
 
-await conn.reply(m.chat, `🍄 *El token te permite iniciar sesion en otros bots, recomendamos no compartirlo con nadie*\n\nTu token es:`, m, rcanal)
+await conn.reply(m.chat, `💎 *El token te permite iniciar sesion en otros bots, recomendamos no compartirlo con nadie*\n\nTu token es:`, m, fake)
 await conn.reply(m.chat, token, m, fake)
 } else {
-await conn.reply(m.chat, `🚩 *No tienes ningun token activo, usa !jadibot para crear uno*`, m, fake)
+await conn.reply(m.chat, `🩵 *No tienes ningun token activo, usa !jadibot para crear uno*`, m, fake)
 }
 
 }
