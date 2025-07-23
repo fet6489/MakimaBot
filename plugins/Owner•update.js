7062ff88ee32816760f5d02dbd62f03e638491f3
mantis-has <mantisbotmd@gmail.com>
2025-07-22 21:54:50 -0400

    Actualizar Owner•update.js

--- plugins/Owner•update.js
+++ plugins/Owner•update.js
@@ -5,11 +5,11 @@ try {
 await m.react(rwait)
 if (conn.user.jid == conn.user.jid) {
 let stdout = execSync('git pull' + (m.fromMe && text ? ' ' + text : ''))
-await conn.reply(m.chat, stdout.toString(), m, rcanal)
+await conn.reply(m.chat, stdout.toString(), m, fake)
 await m.react(done)}
 } catch (e) {
 await m.react(error)
-await m.reply('🚩 Se han hecho cambios locales qué entran en conflicto con las Actualizaciones del Repositorio, Para actualizar, reinstala el Bot o realiza las actualizaciones manualmente.')
+await m.reply('🩵 Se han hecho cambios locales qué entran en conflicto con las Actualizaciones del Repositorio, Para actualizar, reinstala el Bot o realiza las actualizaciones manualmente.')
 }}
 
 handler.help = ['update', 'actualizar']
