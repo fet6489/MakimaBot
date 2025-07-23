2e9cbb7df13e74d68c49583da1ceae0fa1f3cb1b
mantis-has <mantisbotmd@gmail.com>
2025-07-23 02:33:30 -0400

    Actualizar descargas-xnxxdl.js

--- plugins/descargas-xnxxdl.js
+++ plugins/descargas-xnxxdl.js
@@ -1,78 +1,47 @@
-import fetch from 'node-fetch';
-import cheerio from 'cheerio';
+import axios from 'axios';
+const userRequests = {};
 
-const handler = async (m, {conn, args, command, usedPrefix}) => {
-  if (!db.data.chats[m.chat].nsfw && m.isGroup) {
-    return conn.reply(m.chat, `${emoji} El contenido *NSFW* está desactivado en este grupo.\n> Un administrador puede activarlo con el comando » *#nsfw on*`, m);
-  }
-  
-  if (!args[0]) {
-    return conn.reply(m.chat, `${emoji} Por favor, envía un link de Xnxx para descargar el video.\nUso: ${usedPrefix}${command} <link de Xnxx>`, m);
-  }
-  
-  try {
-    await conn.reply(m.chat, `${emoji} El vídeo está siendo procesado, espere un momento...\n\n- El tiempo de envío depende del peso y duración del video.`, m);
-    let xnxxLink = '';
-    
-    if (args[0].includes('xnxx')) {
-      xnxxLink = args[0];
-    } else {
-      const index = parseInt(args[0]) - 1;
-      if (index >= 0) {
-        if (Array.isArray(global.videoListXXX) && global.videoListXXX.length > 0) {
-          const matchingItem = global.videoListXXX.find((item) => item.from === m.sender);
-          if (matchingItem) {
-            if (index < matchingItem.urls.length) {
-              xnxxLink = matchingItem.urls[index];
-            } else {
-              throw `${emoji2} No se encontró un enlace para ese número, por favor ingrese un número entre el 1 y el ${matchingItem.urls.length}.`;
-            }
-          } else {
-            throw `${emoji2} Para poder usar este comando de esta forma (${usedPrefix + command} <numero>), por favor realiza la búsqueda con el comando ${usedPrefix}xnxxsearch <texto>`;
-          }
-        } else {
-          throw `${emoji2} Para poder usar este comando de esta (${usedPrefix + command} <numero>), por favor realiza la búsqueda con el comando ${usedPrefix}xnxxsearch <texto>`;
-        }
-      }
-    }
-
-    const res = await xnxxdl(xnxxLink);
-    const json = await res.result.files;
-    conn.sendMessage(m.chat, {document: {url: json.high}, mimetype: 'video/mp4', fileName: res.result.title}, {quoted: m});
-  } catch (error) {
-    return conn.reply(m.chat, `${msm} Ocurrió un error.\n\n- El enlace debe ser similar a:\n◉ https://www.xnxx.com/video-14lcwbe8/rubia_novia_follada_en_cuarto_de_bano\n\nDetalles del error: ${error}`, m);
-  }
-};
-
-handler.command = ['xnxxdl'];
+let handler = async (m, { conn, usedPrefix, command, text }) => {
+if (!text) throw `*⚠️ ${await tr("Ingresa el nombre del video que buscas")}.*\n${await tr("Ejemplo")}: ${usedPrefix + command} emilia_mernes`
+if (userRequests[m.sender]) return m.reply(`⏳ ${await tr("*Espera...* Ya hay una solicitud en proceso. Por favor, espera a que termine antes de hacer otra.")}`)
+userRequests[m.sender] = true;
+m.react("⏳")
+try {
+let resultTxt = await tr("Resultados para")
+let { data: response } = await axios.get(`${apis}/search/tiktoksearch?query=${text}`);
+if (!response || !response.meta || !Array.isArray(response.meta) || response.meta.length === 0) return m.reply(`❌ ${await tr("No se encontraron resultados para")} "${text}".`);
+let searchResults = response.meta;
+shuffleArray(searchResults);
+let selectedResults = searchResults.slice(0, 5);
+if (m.isWABusiness) {
+const medias = selectedResults.map(result => ({type: "video", data: { url: result.hd }}));
+await conn.sendAlbumMessage(m.chat, medias, `✅ ${resultTxt}: ${text}`, m);
+m.react("✅️");
+} else {
+let messages = selectedResults.map(result => [``,
+`${result.title}`, 
+result.hd
+]);
+await conn.sendCarousel(m.chat, `✅ ${resultTxt}: ${text}`, "🔍 TikTok Search", messages, m);
+m.react("✅️");
+}
+} catch (error) {
+m.react("❌️")
+console.error(error);    
+} finally {
+delete userRequests[m.sender];
+}};
+handler.help = ['tiktoksearch <texto>'];
+handler.tags = ['downloader'];
+handler.command = ['tiktoksearch', 'ttsearch'];
 handler.register = true;
-handler.group = false;
-handler.coin = 10;
+handler.limit = 4;
 
 export default handler;
 
-async function xnxxdl(URL) {
-  return new Promise((resolve, reject) => {
-    fetch(`${URL}`, {method: 'get'}).then((res) => res.text()).then((res) => {
-      const $ = cheerio.load(res, {xmlMode: false});
-      const title = $('meta[property="og:title"]').attr('content');
-      const duration = $('meta[property="og:duration"]').attr('content');
-      const image = $('meta[property="og:image"]').attr('content');
-      const videoType = $('meta[property="og:video:type"]').attr('content');
-      const videoWidth = $('meta[property="og:video:width"]').attr('content');
-      const videoHeight = $('meta[property="og:video:height"]').attr('content');
-      const info = $('span.metadata').text();
-      const videoScript = $('#video-player-bg > script:nth-child(6)').html();
-      const files = {
-        low: (videoScript.match('html5player.setVideoUrlLow\\(\'(.*?)\'\\);') || [])[1],
-        high: videoScript.match('html5player.setVideoUrlHigh\\(\'(.*?)\'\\);' || [])[1],
-        HLS: videoScript.match('html5player.setVideoHLS\\(\'(.*?)\'\\);' || [])[1],
-        thumb: videoScript.match('html5player.setThumbUrl\\(\'(.*?)\'\\);' || [])[1],
-        thumb69: videoScript.match('html5player.setThumbUrl169\\(\'(.*?)\'\\);' || [])[1],
-        thumbSlide: videoScript.match('html5player.setThumbSlide\\(\'(.*?)\'\\);' || [])[1],
-        thumbSlideBig: videoScript.match('html5player.setThumbSlideBig\\(\'(.*?)\'\\);' || [])[1]
-      };
-      resolve({status: 200, result: {title, URL, duration, image, videoType, videoWidth, videoHeight, info, files}});
-    }).catch((err) => reject({code: 503, status: false, result: err}));
-  });
-}
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+  }
