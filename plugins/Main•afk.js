9f2203a7e373444d26fa329ffe93f6a3db1b3e2e
mantis-has <mantisbotmd@gmail.com>
2025-07-23 03:45:48 -0400

    Actualizar Main•afk.js

--- plugins/Main•afk.js
+++ plugins/Main•afk.js
@@ -2,8 +2,8 @@ const handler = async (m, {text}) => {
 const user = global.db.data.users[m.sender];
 user.afk = + new Date;
 user.afkReason = text;
-conn.reply(m.chat, `🚩 *El Usuario ${conn.getName(m.sender)} Estará Inactivo*\n\n*Motivo: ${text ? ': ' + text : 'Sin Especificar!'}*
-`, m, rcanal);
+conn.reply(m.chat, `🩵 *El Usuario ${conn.getName(m.sender)} Estará Inactivo*\n\n*Motivo: ${text ? ': ' + text : 'Sin Especificar!'}*
+`, m, fake);
 };
 handler.help = ['afk [alasan]'];
 handler.tags = ['main'];
