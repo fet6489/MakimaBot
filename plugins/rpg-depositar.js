8ca8b78e08ff62a8559787458ba71a2b0c7aac55
mantis-has <mantisbotmd@gmail.com>
2025-07-23 01:46:59 -0400

    Actualizar rpg-depositar.js

--- plugins/rpg-depositar.js
+++ plugins/rpg-depositar.js
@@ -2,25 +2,25 @@ import db from '../lib/database.js'
 
 let handler = async (m, { args }) => {
 let user = global.db.data.users[m.sender]
-if (!args[0]) return m.reply('🩵 Ingresa la cantidad de *Diamantes* que deseas Depositar.')
+if (!args[0]) return m.reply('🩵 Ingresa la cantidad de *Diamantes* que quieres Depositar.')
 if ((args[0]) < 1) return m.reply('🩵 Ingresa una cantidad válida de *Diamantes*.')
 if (args[0] == 'all') {
-let count = parseInt(user.monedas)
-user.monedas -= count * 1
+let count = parseInt(user.chocolates)
+user.chocolates -= count * 1
 user.bank += count * 1
 await m.reply(`Depositaste *${count} Diamantes* al Banco.`)
 return !0
 }
 if (!Number(args[0])) return m.reply('🩵 La cantidad deve ser un Numero.')
 let count = parseInt(args[0])
-if (!user.monedas) return m.reply('No tienes *Diamantes* en la Cartera.')
-if (user.monedas < count) return m.reply(`Solo tienes *${user.monedas} Monedas 🪙* en la Cartera.`)
-user.monedas -= count * 1
+if (!user.chocolates) return m.reply('No tienes *Diamantes* en la Cartera.')
+if (user.chocolates < count) return m.reply(`Solo tienes *${user.chocolates} Diamantes* en la Cartera.`)
+user.chocolates -= count * 1
 user.bank += count * 1
-await m.reply(`👨‍💻 Depositaste *${count} Diamantes* al Banco.`)}
+await m.reply(`Depositaste *${count} Diamantes* al Banco.`)}
 
 handler.help = ['depositar']
 handler.tags = ['rpg']
-handler.command = ['deposit', 'depositar', 'd', 'aguardar']
+handler.command = ['deposit', 'depositar', 'dep', 'aguardar']
 handler.register = true
 export default handler 
