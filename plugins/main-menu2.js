870961b1b9d8a8982a29cc21c512cca079a7b8c2
mantis-has <mantisbotmd@gmail.com>
2025-07-23 01:17:15 -0400

    Actualizar main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -11,11 +11,11 @@ let handler = async (m, {conn, usedPrefix}) => {
    let user = global.db.data.users[who]
    let name = conn.getName(who);
    let txt = (`${who == m.sender ? `╭━〔  ECONOMIA  〕⬣\n┋ *Usuario:* ${name}\n┋ *Diamantes En Cartera*: ${user.chocolates}\n┋ *Diamantes En Banco*: ${user.bank}\n┋ *Experiencia:* ${user.exp}\n┋ *Nivel:* ${user.level}\n┋ *Rol:* ${user.role}\n┋ *Fecha:* ${new Date().toLocaleString('id-ID')}\n╰━━━━━━━━━━━━⬣` : `╭━〔  ${global.packname}  〕⬣\n┋ *Usuario:* @${who.split('@')[0]}\n┋ *Diamantes En Cartera*: ${user.chocolates}\n┋ *Diamantes En Banco*: ${user.bank}\n┋ *Experiencia:* ${user.exp}\n┋ *Nivel:* ${user.level}\n┋ *Rol:* ${user.role}\n┋ *Fecha:* ${new Date().toLocaleString('id-ID')}\n╰━━━━━━━━━━━━⬣`}`)
-await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, {mentions: [who] }, rcanal)
+await conn.sendFile(m.chat, img, 'thumbnail.jpg', txt, m, null, {mentions: [who] }, fake)
 }
 
 handler.help = ['bank']
 handler.tags = ['rpg']
-handler.command = ['bank', 'banco'] 
+handler.command = ['bal', 'bal'] 
 handler.register = true 
 export default handler 
