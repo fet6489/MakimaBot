d5d2c3b953023c1c9bc83fe30fccd8fd285cf52b
mantis-has <mantisbotmd@gmail.com>
2025-07-23 01:23:55 -0400

    Actualizar Game•scared.js

--- plugins/Game•scared.js
+++ plugins/Game•scared.js
@@ -1,56 +1,64 @@
-//Codígo creado por Destroy wa.me/584120346669
+let cooldowns = {}
 
-import fs from 'fs';
-import path from 'path';
+let handler = async (m, { conn, text, command, usedPrefix }) => {
+let users = global.db.data.users
+let senderId = m.sender
+let senderName = conn.getName(senderId)
 
-let handler = async (m, { conn, usedPrefix }) => {
-    let who;
-
-    // Verificamos si se menciona a alguien o se cita un mensaje
-    if (m.mentionedJid.length > 0) {
-        who = m.mentionedJid[0]; // Si hay mención, usamos esa
-    } else if (m.quoted) {
-        who = m.quoted.sender; // Si se cita un mensaje, usamos el emisor de ese mensaje
-    } else {
-        who = m.sender; // En caso contrario, usamos el emisor
-    }
-
-    let name = conn.getName(who); // Nombre de la persona mencionada o del emisor
-    let name2 = conn.getName(m.sender); // Nombre del usuario que envía el comando
-    m.react('😨');
-
-    // Construimos el mensaje dependiendo de si hay una mención o no
-    let str;
-    if (m.mentionedJid.length > 0) {
-        str = `\`${name2}\` Está asustad﹫ de \`${name || who}\`.`; // Usamos nombre agendado o número si no está agendado
-    } else if (m.quoted) {
-        str = `\`${name2}\` Está asustad﹫ de \`${name || who}\`.`; // Mensaje cuando se cita a otro usuario
-    } else {
-        str = `\`${name2}\` Está asustad﹫.`.trim();
-    }
-    
-    if (m.isGroup) {
-        let pp = 'https://telegra.ph/file/9c1e963fa4d8269fb17a7.mp4'; 
-        let pp2 = 'https://telegra.ph/file/0c802b4fa616aaf1da229.mp4'; 
-        let pp3 = 'https://telegra.ph/file/d0b166d9a363765e51657.mp4';
-        let pp4 = 'https://telegra.ph/file/eae6dd9d45e45fe3a95ab.mp4';
-        let pp5 = 'https://telegra.ph/file/1785e535a4463c2a337c5.mp4';
-        let pp6 = 'https://telegra.ph/file/c1673b418bc61db1e51a0.mp4';
-        let pp7 = 'https://quhttps://telegra.ph/file/9774e1d74c3abf083ae01.mp4.ax/EDZBg.mp4';
-        let pp8 = 'https://telegra.ph/file/dcde646a58d8e9bf44867.mp4';
-        
-        const videos = [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8];
-        const video = videos[Math.floor(Math.random() * videos.length)];
-        
-        // Enviamos el mensaje con el video y el mensaje correspondiente
-        let mentions = [who]; // Mencionamos al usuario que se ha citado o mencionado
-        conn.sendMessage(m.chat, { video: { url: video }, gifPlayback: true, caption: str, mentions }, { quoted: m });
-    }
+let tiempo = 5 * 60
+if (cooldowns[m.sender] && Date.now() - cooldowns[m.sender] < tiempo * 1000) {
+let tiempo2 = segundosAHMS(Math.ceil((cooldowns[m.sender] + tiempo * 1000 - Date.now()) / 1000))
+m.reply(`🩵 Ya has cometido un Crimen recientemente, espera ⏱️ *${tiempo2}* para cometer tu próximo Crimen y evitar ser atrapado.`)
+return
 }
+cooldowns[m.sender] = Date.now()
+let senderChocolates = users[senderId].chocolates || 0
+let randomUserId = Object.keys(users)[Math.floor(Math.random() * Object.keys(users).length)]
+while (randomUserId === senderId) {
+randomUserId = Object.keys(users)[Math.floor(Math.random() * Object.keys(users).length)]}
+let randomUserChocolates = users[randomUserId].chocolates || 0
+let minAmount = 15
+let maxAmount = 50
+let amountTaken = Math.floor(Math.random() * (maxAmount - minAmount + 1)) + minAmount
+let randomOption = Math.floor(Math.random() * 3)
+switch (randomOption) {
+case 0:
+users[senderId].chocolates += amountTaken
+users[randomUserId].chocolates -= amountTaken
+conn.sendMessage(m.chat, {
+text: `🩵¡Lograste cometer tu crimen con exito!, acabas de robar *${amountTaken} Diamantes* a @${randomUserId.split("@")[0]}\n\nSe suman *+${amountTaken} Diamantes* a ${senderName}.`,
+contextInfo: { 
+mentionedJid: [randomUserId],
+}}, { quoted: m })
+break
+case 1:
+let amountSubtracted = Math.min(Math.floor(Math.random() * (senderChocolates - minAmount + 1)) + minAmount, maxAmount)
+users[senderId].chocolates -= amountSubtracted
+conn.reply(m.chat, `🩵 No fuiste cuidadoso y te atraparon mientras cometias tu crimen, se restaron *-${amountSubtracted} Diamantes* a ${senderName}.`, m, fake)
+break
+case 2:
+let smallAmountTaken = Math.min(Math.floor(Math.random() * (randomUserChocolates / 2 - minAmount + 1)) + minAmount, maxAmount)
+users[senderId].chocolates += smallAmountTaken
+users[randomUserId].chocolates -= smallAmountTaken
+conn.sendMessage(m.chat, {
+text: `🩵 Lograste cometer tu crimen con exito, pero te descubrieron y solo lograste tomar *${smallAmountTaken} Diamantes* de @${randomUserId.split("@")[0]}\n\nSe suman *+${smallAmountTaken} Diamantes* a ${senderName}.`,
+contextInfo: { 
+mentionedJid: [randomUserId],
+}}, { quoted: m })
+break
+}
+global.db.write()}
 
-handler.help = ['scared/asustada @tag'];
-handler.tags = ['emox'];
-handler.command = ['scared', 'asustada'];
-handler.group = true;
+handler.tags = ['rpg']
+handler.help = ['crimen']
+handler.command = ['crimen', 'crime']
+handler.register = true
 
-export default handler;
+export default handler
+
+function segundosAHMS(segundos) {
+let horas = Math.floor(segundos / 3600)
+let minutos = Math.floor((segundos % 3600) / 60)
+let segundosRestantes = segundos % 60
+return `${minutos} minutos y ${segundosRestantes} segundos`
+}
