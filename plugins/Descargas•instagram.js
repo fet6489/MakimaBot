e83c3f69e0349acb12f8dc63ca76e5d7d3b21eb6
fet6489 <xdjulmi@gmail.com>
2025-07-27 01:01:53 -0400

    Actualizar Descargas•instagram.js

--- plugins/Descargas•instagram.js
+++ plugins/Descargas•instagram.js
@@ -2,10 +2,10 @@ import { igdl } from "ruhend-scraper"
 
 let handler = async (m, { args, conn }) => { 
 if (!args[0]) {
-return conn.reply(m.chat, '🚩 Ingresa un link de Instagram.', m, rcanal)}
+return conn.reply(m.chat, '🩵 Ingresa un link de Instagram.', m, fake)}
 try {
 await m.react(rwait)
-conn.reply(m.chat, `🕒 *Enviando El Video...*`, m, {
+conn.reply(m.chat, `🩵 *Enviando El Video...*`, m, fake, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: dev,
@@ -16,10 +16,10 @@ let res = await igdl(args[0])
 let data = res.data       
 for (let media of data) {
 await new Promise(resolve => setTimeout(resolve, 2000))           
-await conn.sendFile(m.chat, media.url, 'instagram.mp4', '🚩 *Video de instagram.*\n' + textbot, m)
+await conn.sendFile(m.chat, media.url, 'instagram.mp4', '🩵 *Video de instagram.*\n' + textbot, m, fake)
 }} catch {
 await m.react(error)
-conn.reply(m.chat, '🚩 Ocurrió un error.', m, fake)}}
+conn.reply(m.chat, '🩵 Ocurrió un error.', m, fake)}}
 
 handler.command = ['instagram', 'ig']
 handler.tags = ['descargas']
